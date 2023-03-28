733d8a47e970e26747e41b654438499056ab4929
Daniel Williams <equipoise@gmail.com>
2023-03-28 19:00:13 -0400

    [format] applied black format to files

--- list.py
+++ list.py
@@ -8,8 +8,12 @@ from wand.image import Image
 import sys
 import zipfile
 
-_wsl_base_path = Path('/mnt/d/project/cnctd/data/textures/textures_td_srgb/DATA/ART/TEXTURES/SRGB/TIBERIAN_DAWN')
-_win_base_path = Path('c:d\project\cnctd\data\textures\textures_td_srgb\DATA\ART\TEXTURES\SRGB\TIBERIAN_DAWN')
+_wsl_base_path = Path(
+    "/mnt/d/project/cnctd/data/textures/textures_td_srgb/DATA/ART/TEXTURES/SRGB/TIBERIAN_DAWN"
+)
+_win_base_path = Path(
+    "c:d\project\cnctd\data\textures\textures_td_srgb\DATA\ART\TEXTURES\SRGB\TIBERIAN_DAWN"
+)
 _base_path = None
 
 _output_path = None
@@ -20,43 +24,53 @@ if os.path.exists(_wsl_base_path):
 if os.path.exists(_win_base_path):
     _base_path = _win_base_path
 
-_null_filter = re.compile('^\n$')
+_null_filter = re.compile("^\n$")
 
-def action_none( _ ):
+
+def action_none(_):
     return
 
 
-def action_print( name : str ):
+def action_print(name: str):
     print(name)
 
-_png_filter = re.compile('.+\.png$')
-_tga_filter = re.compile('.+\.TGA$')
-_dds_filter = re.compile('.+\.DDS$')
+
+_png_filter = re.compile(".+\.png$")
+_tga_filter = re.compile(".+\.TGA$")
+_dds_filter = re.compile(".+\.DDS$")
+
 
 # Uses package:
 # https://docs.wand-py.org/en/0.6.11/
-def action_dds_to_png( name: str ):
-    from_suffix = 'DDS'.upper()
-    to_suffix = 'png'.lower()
+def action_dds_to_png(name: str):
+    from_suffix = "DDS".upper()
+    to_suffix = "png".lower()
     if name.upper().endswith(from_suffix):
         with Image(filename=name) as read_image:
             with read_image.clone() as write_image:
                 write_image.format = to_suffix
-                write_image_name = str(_output_path.joinpath( Path(name).stem + '.' + to_suffix))
+                write_image_name = str(
+                    _output_path.joinpath(Path(name).stem + "." + to_suffix)
+                )
                 print("<<<: " + write_image_name)
                 write_image.save(filename=write_image_name)
 
-_zip_filter = re.compile('.+\.ZIP$')
-def action_unzip( zip_file_path: Path ):
+
+_zip_filter = re.compile(".+\.ZIP$")
+
+
+def action_unzip(zip_file_path: Path):
     extract_dir = zip_file_path.parent
-    with zipfile.ZipFile( zip_file_path, 'r') as zip_ref:
+    with zipfile.ZipFile(zip_file_path, "r") as zip_ref:
         zip_ref.extractall(extract_dir)
+
 
 _matches_found = 0
 _done = False
 
-def apply( curpath: Path, filter: re.Pattern, action=None, **kwargs ):
-    if isinstance( curpath, str):
+
+def apply(curpath: Path, filter: re.Pattern, action=None, **kwargs):
+    if isinstance(curpath, str):
         curpath = Path(curpath)
 
     indent = kwargs.get("indent", 4)
@@ -67,16 +81,16 @@ def apply( curpath: Path, filter: re.Pattern, action=None, **kwargs ):
     if 2 < verbose:
         print(f"  @:path: {curpath}: {len(os.listdir(curpath))}")
 
-    for subdir in os.listdir( curpath ):
+    for subdir in os.listdir(curpath):
         global _done
         if _done:
             return True
-         
+
         subpath = curpath.joinpath(subdir)
         if subpath.is_dir():
             if 0 < verbose:
                 print(f"{' ':{indent}}==> descending into: {subdir} ({str(subpath)})")
-            apply( subpath, filter, action, **kwargs )
+            apply(subpath, filter, action, **kwargs)
 
         elif filter.match(subdir):
             if 1 < verbose:
@@ -94,16 +108,17 @@ def apply( curpath: Path, filter: re.Pattern, action=None, **kwargs ):
 
     return True
 
-if "__main__" == __name__:
 
-    parser = argparse.ArgumentParser(prog='extractor',
-                    description='extract certain files from game archive')
-    parser.add_argument('-b','--base-path')
-    parser.add_argument('-s', '--search-path')
-    parser.add_argument('-o','--output-path')
-    parser.add_argument('-v', '--verbose', action='count', default=0)
+if "__main__" == __name__:
+    parser = argparse.ArgumentParser(
+        prog="extractor", description="extract certain files from game archive"
+    )
+    parser.add_argument("-b", "--base-path")
+    parser.add_argument("-s", "--search-path")
+    parser.add_argument("-o", "--output-path")
+    parser.add_argument("-v", "--verbose", action="count", default=0)
     args = parser.parse_args()
-    
+
     if args.base_path:
         base_path = args.base_path
     else:
@@ -111,17 +126,16 @@ if "__main__" == __name__:
 
     if args.search_path:
         search_path = args.search_path
-    else:  
-        search_path = base_path.joinpath('TERRAIN','DESERT')
+    else:
+        search_path = base_path.joinpath("TERRAIN", "DESERT")
 
     if args.output_path:
         _output_path = args.output_path
-    else:  
-        _output_path = Path('data/stage/')
+    else:
+        _output_path = Path("data/stage/")
 
     verbose = args.verbose
     if 0 < verbose:
         print(f"Starting at: {search_path}")
 
-    apply( search_path, filter=_dds_filter, action=action_dds_to_png, verbose=verbose )
-
+    apply(search_path, filter=_dds_filter, action=action_dds_to_png, verbose=verbose)
--- stitch.py
+++ stitch.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
+# Standard Library Imports
+import argparse
 from pathlib import Path
-import wand
-from wand.image import Image
 import sys
 
-
+# 3rd Party Imports
+import wand
+from wand.image import Image
 
 
 _stage_path = Path("data/stage/")
@@ -15,18 +17,27 @@ _output_path = Path("output/terrain/desert")
 _write_path = Path("output/terrain/desert")
 
 # load some test paths:
-test_paths = ["/mnt/d/project/cnctd/data/stage/B6.DES-0000.png","/mnt/d/project/cnctd/data/stage/B1.DES-0000.png","/mnt/d/project/cnctd/data/stage/B2.DES-0000.png","/mnt/d/project/cnctd/data/stage/B2.DES-0001.png","/mnt/d/project/cnctd/data/stage/B4.DES-0000.png","/mnt/d/project/cnctd/data/stage/B5.DES-0000.png"]
+test_paths = [
+    "/mnt/d/project/cnctd/data/stage/B6.DES-0000.png",
+    "/mnt/d/project/cnctd/data/stage/B1.DES-0000.png",
+    "/mnt/d/project/cnctd/data/stage/B2.DES-0000.png",
+    "/mnt/d/project/cnctd/data/stage/B2.DES-0001.png",
+    "/mnt/d/project/cnctd/data/stage/B4.DES-0000.png",
+    "/mnt/d/project/cnctd/data/stage/B5.DES-0000.png",
+]
 
+
+# Constant For Command & Conque -- Tiberian Dawn
+_tile_height = 128
+_tile_width = 128
 
 # ================
 # DEBUG values:
-tile_height = 128
-tile_width = tile_height
-tilesheet_height = 2*tile_height
-tilesheet_width = 3*tile_height
+tilesheet_height = 2 * _tile_height
+tilesheet_width = 3 * _tile_height
 # DEBUG
 
-tilesheet = Image( width=tilesheet_width, height=tilesheet_height )
+tilesheet = Image(width=tilesheet_width, height=tilesheet_height)
 
 # ================
 each_path = iter(test_paths)
@@ -36,15 +47,49 @@ for row in range(2):
     pixels_from_left = 0
     for col in range(3):
         curpath = next(each_path)
-        print(f"        [{col},{row}]: @({pixels_from_top},{pixels_from_left}): {curpath}")
+        print(
+            f"        [{col},{row}]: @({pixels_from_top},{pixels_from_left}): {curpath}"
+        )
         curimg = Image(filename=curpath)
         tilesheet.composite(curimg, top=pixels_from_top, left=pixels_from_left)
-                                
-        pixels_from_left += tile_width
+
+        pixels_from_left += _tile_width
 
     print(f"    << Finished Row: {row}")
-    pixels_from_top += tile_height
+    pixels_from_top += _tile_height
 
 print(f"::Finished Compositing Tiles.")
 
-tilesheet.save(filename='test.composite.exact.png')
+tilesheet.save(filename="test.composite.exact.png")
+
+
+if "__main__" == __name__:
+    parser = argparse.ArgumentParser(
+        prog="extractor", description="extract certain files from game archive"
+    )
+    parser.add_argument("-b", "--base-path")
+    parser.add_argument("-s", "--search-path")
+    parser.add_argument("-o", "--output-path")
+    parser.add_argument("-v", "--verbose", action="count", default=0)
+    args = parser.parse_args()
+
+    if args.base_path:
+        base_path = args.base_path
+    else:
+        base_path = Path(_base_path)
+
+    if args.search_path:
+        search_path = args.search_path
+    else:
+        search_path = base_path.joinpath("TERRAIN", "DESERT")
+
+    if args.output_path:
+        _output_path = args.output_path
+    else:
+        _output_path = Path("data/stage/")
+
+    verbose = args.verbose
+    if 0 < verbose:
+        print(f"Starting at: {search_path}")
+
+    apply(search_path, filter=_dds_filter, action=action_dds_to_png, verbose=verbose)
